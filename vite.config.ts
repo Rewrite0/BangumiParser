abb132ec4803246c37d6152e82e0b1b09cd82b01
Rewrite00 <747556795@qq.com>
2023-02-07 17:39:21 +0800

    fix: 丢失types

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  plugins: [dts()],
   esbuild: {
     drop: ['console', 'debugger'],
   },
@@ -9,6 +11,7 @@ export default defineConfig({
       entry: 'main.ts',
       name: 'bangumi-title-parser',
       fileName: 'index',
+      formats: ['es'],
     },
   },
 });
